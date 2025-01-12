58eb71845c76ee6d2b59fdc0e340e56ad2c194ff
Aniket Ghosh <aniketghosh255@gmail.com>
2025-01-12 16:27:25 +0530

    Delete dishify directory

--- dishify/recipegenerator/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class RecipeRequest(models.Model):
-    ingredients = models.TextField()
-    cuisine = models.CharField(max_length=50, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
